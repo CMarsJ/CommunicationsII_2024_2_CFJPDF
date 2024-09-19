257ff91f23ad92ecd213e363133491c3b585e98f
Carlos Carreño J. <83018836+CMarsJ@users.noreply.github.com>
2024-09-19 10:27:10 -0500

    Cambios 3

--- Practica_2/GNURadio/randombinayrectsignal - Punto3/prac3a.py
+++ Practica_2/GNURadio/randombinayrectsignal - Punto3/prac3a.py
@@ -251,7 +251,9 @@ class prac3a(gr.top_block, Qt.QWidget):
         # Connections
         ##################################################
         self.connect((self.analog_const_source_x_0_0, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_null_sink_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.analog_random_source_x_0, 0), (self.blocks_char_to_float_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.blocks_char_to_float_0, 0), (self.blocks_add_xx_0, 1))
@@ -259,9 +261,7 @@ class prac3a(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.interp_fir_filter_xxx_0, 0))
         self.connect((self.blocks_multiply_const_vxx_1, 0), (self.qtgui_vector_sink_f_0, 0))
         self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_stream_to_vector_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_null_sink_0, 0))
         self.connect((self.epy_block_0, 0), (self.blocks_multiply_const_vxx_1, 0))
         self.connect((self.fft_vxx_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
         self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_throttle_0, 0))
